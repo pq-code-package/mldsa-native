fa6c86929c74891ade973beab2a079ea3a4ae7ef
Jake Massimo <jakemas@amazon.com>
2025-05-08 21:33:50 +0000

    CBMC: Add proof and contract for polyvecl_uniform_eta
    Corrections for proof of this function:
    1. Disable unsigned-overflow check for increment of nonce - overflow
       is expected and required.
    2. Simplify loop invariant and add assigns contract for loop.
    
    Signed-off-by: Jake Massimo <jakemas@amazon.com>

--- mldsa/polyvec.c
+++ mldsa/polyvec.c
@@ -39,13 +39,33 @@ void polyvec_matrix_pointwise_montgomery(polyveck *t,
 /**************************************************************/
 
 void polyvecl_uniform_eta(polyvecl *v, const uint8_t seed[MLDSA_CRHBYTES],
-                          uint16_t nonce)
+                          const uint16_t nonce)
 {
   unsigned int i;
-
+  uint16_t n = nonce;
   for (i = 0; i < MLDSA_L; ++i)
+  __loop__(
+    assigns(i, n, object_whole(v))
+    invariant(i <= MLDSA_L)
+    invariant(forall(k1, 0, i,
+      array_abs_bound(v->vec[k1].coeffs, 0, MLDSA_N, MLDSA_ETA + 1))))
   {
-    poly_uniform_eta(&v->vec[i], seed, nonce++);
+    poly t;
+    poly_uniform_eta(&t, seed, n);
+
+#ifdef CBMC
+#pragma CPROVER check push
+#pragma CPROVER check disable "unsigned-overflow"
+#endif
+    n++;
+#ifdef CBMC
+#pragma CPROVER check pop
+#endif
+
+    /* Full struct assignment from local variables to simplify proof */
+    /* TODO: eliminate once CBMC resolves
+     * https://github.com/diffblue/cbmc/issues/8617 */
+    v->vec[i] = t;
   }
 }
 
