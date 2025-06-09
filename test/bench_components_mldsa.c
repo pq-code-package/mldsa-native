8e23934c2b0376b867d729f4232048007fb1a698
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-06-09 16:59:22 +0800

    Extend component benchmarks
    Resolves https://github.com/pq-code-package/mldsa-native/issues/308
    
    This commit adds component benchmarks for various poly and polyvec functions
    that have been identified as potential optimizaton targets.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- test/bench_components_mldsa.c
+++ test/bench_components_mldsa.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include "../mldsa/ntt.h"
 #include "../mldsa/poly.h"
+#include "../mldsa/polyvec.h"
 #include "../mldsa/randombytes.h"
 #include "hal.h"
 
@@ -26,6 +27,9 @@ static int cmp_uint64_t(const void *a, const void *b)
   for (i = 0; i < NTESTS; i++)                          \
   {                                                     \
     randombytes((uint8_t *)data0, sizeof(data0));       \
+    randombytes((uint8_t *)data1, sizeof(data1));       \
+    randombytes((uint8_t *)data2, sizeof(data2));       \
+    randombytes((uint8_t *)data3, sizeof(data3));       \
     for (j = 0; j < NWARMUP; j++)                       \
     {                                                   \
       code;                                             \
@@ -44,14 +48,46 @@ static int cmp_uint64_t(const void *a, const void *b)
 
 static int bench(void)
 {
-  MLD_ALIGN int32_t data0[256];
+  MLD_ALIGN int32_t data0[MLDSA_N * MLDSA_K];
+  MLD_ALIGN int32_t data1[MLDSA_N * MLDSA_K];
+  MLD_ALIGN int32_t data2[MLDSA_N * MLDSA_K];
+  MLD_ALIGN int32_t data3[MLDSA_N * MLDSA_K];
   uint64_t cyc[NTESTS];
   unsigned i, j;
   uint64_t t0, t1;
 
-  /* ntt */
   BENCH("poly_ntt", poly_ntt((poly *)data0))
   BENCH("poly_invntt_tomont", poly_invntt_tomont((poly *)data0))
+
+  BENCH("poly_pointwise_montgomery",
+        poly_pointwise_montgomery((poly *)data0, (poly *)data1, (poly *)data2))
+  BENCH("poly_reduce", poly_reduce((poly *)data0))
+  BENCH("poly_caddq", poly_caddq((poly *)data0))
+  BENCH("poly_add", poly_add((poly *)data0, (poly *)data1))
+  BENCH("poly_sub", poly_sub((poly *)data0, (poly *)data1))
+  BENCH("poly_shiftl", poly_shiftl((poly *)data0))
+  BENCH("poly_power2round",
+        poly_power2round((poly *)data0, (poly *)data1, (poly *)data2))
+  BENCH("poly_decompose",
+        poly_decompose((poly *)data0, (poly *)data1, (poly *)data2))
+  BENCH("poly_make_hint",
+        poly_make_hint((poly *)data0, (poly *)data1, (poly *)data2))
+  BENCH("poly_use_hint",
+        poly_use_hint((poly *)data0, (poly *)data1, (poly *)data2))
+  BENCH("poly_chknorm", poly_chknorm((poly *)data0, INT32_MAX))
+  BENCH("polyz_unpack", polyz_unpack((poly *)data0, (uint8_t *)data1))
+  BENCH("polyw1_pack", polyw1_pack((uint8_t *)data0, (poly *)data1))
+  BENCH("polyt1_unpack", polyt1_unpack((poly *)data0, (uint8_t *)data1))
+  BENCH("polyt1_unpack", polyt1_unpack((poly *)data0, (uint8_t *)data1))
+  BENCH("poly_uniform", poly_uniform((poly *)data0, (uint8_t *)data1))
+  BENCH("poly_uniform_4x", poly_uniform((poly *)data0, (uint8_t *)data1))
+  BENCH("poly_uniform_eta_4x",
+        poly_uniform_eta_4x((poly *)data0, (poly *)data1, (poly *)data2,
+                            (poly *)data3, (uint8_t *)data1, 0, 1, 2, 3))
+
+  BENCH("polyvecl_pointwise_acc_montgomery",
+        polyvecl_pointwise_acc_montgomery((poly *)data0, (polyvecl *)data1,
+                                          (polyvecl *)data2))
 
   return 0;
 }
