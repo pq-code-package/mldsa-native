f29e99edcadaea082c9ffbbcb9118ac02eb27e1c
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-07-15 09:27:23 +0800

    Inline reduce.c
    This commits inlines reduce.c entirely. The goal is to get performance without
    -flto closer to performance with -flto.
    Namespacing will be added in the next commit.
    
    Note that some of these functions are a potential side-channel leak.
    That needs to be addressed urgently, but it with -flto it was already present
    prior to this commit.
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mldsa/reduce.h
+++ mldsa/reduce.h
@@ -23,6 +23,33 @@
 /* Absolute bound for tight domain of montgomery_reduce() */
 #define MONTGOMERY_REDUCE_STRONG_DOMAIN_MAX ((int64_t)INT32_MIN * -MLDSA_Q)
 
+/*************************************************
+ * Name:        mld_cast_uint32_to_int32
+ *
+ * Description: Cast uint32 value to int32
+ *
+ * Returns:
+ *   input x in     0 .. 2^31-1: returns value unchanged
+ *   input x in  2^31 .. 2^32-1: returns (x - 2^32)
+ **************************************************/
+#ifdef CBMC
+#pragma CPROVER check push
+#pragma CPROVER check disable "conversion"
+#endif
+static MLD_INLINE int32_t mld_cast_uint32_to_int32(uint32_t x)
+{
+  /*
+   * PORTABILITY: This relies on uint32_t -> int32_t
+   * being implemented as the inverse of int32_t -> uint32_t,
+   * which is implementation-defined (C99 6.3.1.3 (3))
+   * CBMC (correctly) fails to prove this conversion is OK,
+   * so we have to suppress that check here
+   */
+  return (int32_t)x;
+}
+#ifdef CBMC
+#pragma CPROVER check pop
+#endif
 
 #define montgomery_reduce MLD_NAMESPACE(montgomery_reduce)
 /*************************************************
@@ -46,7 +73,7 @@
  *
  * Returns r.
  **************************************************/
-int32_t montgomery_reduce(int64_t a)
+static MLD_INLINE int32_t montgomery_reduce(int64_t a)
 __contract__(
   requires(a >= -MONTGOMERY_REDUCE_DOMAIN_MAX && a <= MONTGOMERY_REDUCE_DOMAIN_MAX)
   ensures(return_value >= INT32_MIN && return_value < REDUCE32_DOMAIN_MAX)
@@ -54,7 +81,31 @@ __contract__(
   /* Special case - for stronger input bounds, we can ensure stronger bounds on the output */
   ensures((a >= -MONTGOMERY_REDUCE_STRONG_DOMAIN_MAX && a < MONTGOMERY_REDUCE_STRONG_DOMAIN_MAX) ==>
           (return_value > -MLDSA_Q && return_value < MLDSA_Q))
-);
+)
+{
+  /* check-magic: 58728449 == unsigned_mod(pow(MLDSA_Q, -1, 2^32), 2^32) */
+  const uint64_t QINV = 58728449;
+
+  /*  Compute a*q^{-1} mod 2^32 in unsigned representatives */
+  const uint32_t a_reduced = a & UINT32_MAX;
+  const uint32_t a_inverted = (a_reduced * QINV) & UINT32_MAX;
+
+  /* Lift to signed canonical representative mod 2^16. */
+  const int32_t t = mld_cast_uint32_to_int32(a_inverted);
+
+  int64_t r;
+
+  r = a - ((int64_t)t * MLDSA_Q);
+
+  /*
+   * PORTABILITY: Right-shift on a signed integer is, strictly-speaking,
+   * implementation-defined for negative left argument. Here,
+   * we assume it's sign-preserving "arithmetic" shift right. (C99 6.5.7 (5))
+   */
+  r = r >> 32;
+  return (int32_t)r;
+}
+
 
 #define reduce32 MLD_NAMESPACE(reduce32)
 /*************************************************
@@ -68,12 +119,20 @@ __contract__(
  *
  * Returns r.
  **************************************************/
-int32_t reduce32(int32_t a)
+static MLD_INLINE int32_t reduce32(int32_t a)
 __contract__(
   requires(a <= REDUCE32_DOMAIN_MAX)
   ensures(return_value >= -REDUCE32_RANGE_MAX)
   ensures(return_value <   REDUCE32_RANGE_MAX)
-);
+)
+{
+  int32_t t;
+
+  t = (a + (1 << 22)) >> 23;
+  t = a - t * MLDSA_Q;
+  cassert((t - a) % MLDSA_Q == 0);
+  return t;
+}
 
 #define caddq MLD_NAMESPACE(caddq)
 /*************************************************
@@ -85,14 +144,18 @@ __contract__(
  *
  * Returns r.
  **************************************************/
-int32_t caddq(int32_t a)
+static MLD_INLINE int32_t caddq(int32_t a)
 __contract__(
   requires(a > -MLDSA_Q)
   requires(a < MLDSA_Q)
   ensures(return_value >= 0)
   ensures(return_value < MLDSA_Q)
   ensures(return_value == (a >= 0) ? a : (a + MLDSA_Q))
-);
+)
+{
+  a += (a >> 31) & MLDSA_Q;
+  return a;
+}
 
 
 #endif /* !MLD_REDUCE_H */
