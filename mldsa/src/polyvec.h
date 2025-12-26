efac3a820a9cd132beaa8aa509ebf83f25af8463
willieyz <willie.zhao@chelpis.com>
2025-12-26 18:29:24 +0800

    Fix macro typo: MLK_INTERNAL_API => MLD_INTERNAL_API
    - Correct a macro typo in .clang-format: MLK_INTERNAL_API should be
      MLD_INTERNAL_API. This was detected after extending the macro checker
      to catch incorrectly ported MLK_XXX/MLKEM_XXX macros.
    
    - Fixing the typo caused clang-format to reformat some files
      (packing.h and polyvec.h), changing the macro order from:
    
          MLD_MUST_CHECK_RETURN_VALUE
          MLD_INTERNAL_API
    
      to:
    
          MLD_MUST_CHECK_RETURN_VALUE MLD_INTERNAL_API
    
    - The above reordering is incorrect. According to the reference in
      mlkem-native, the correct macro order is:
    
          MLD_INTERNAL_API
          MLD_MUST_CHECK_RETURN_VALUE
    
      This matches the mlkem-native convention and is the intended
      formatting behavior.
    
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

--- mldsa/src/polyvec.h
+++ mldsa/src/polyvec.h
@@ -123,8 +123,8 @@ __contract__(
  * Returns 0 if norm of all polynomials is strictly smaller than B <=
  * (MLDSA_Q-1)/8 and 0xFFFFFFFF otherwise.
  **************************************************/
-MLD_MUST_CHECK_RETURN_VALUE
 MLD_INTERNAL_API
+MLD_MUST_CHECK_RETURN_VALUE
 uint32_t mld_polyvecl_chknorm(const mld_polyvecl *v, int32_t B)
 __contract__(
   requires(memory_no_alias(v, sizeof(mld_polyvecl)))
@@ -336,8 +336,8 @@ __contract__(
  * Returns 0 if norm of all polynomials are strictly smaller than B <=
  *(MLDSA_Q-1)/8 and 0xFFFFFFFF otherwise.
  **************************************************/
-MLD_MUST_CHECK_RETURN_VALUE
 MLD_INTERNAL_API
+MLD_MUST_CHECK_RETURN_VALUE
 uint32_t mld_polyveck_chknorm(const mld_polyveck *v, int32_t B)
 __contract__(
   requires(memory_no_alias(v, sizeof(mld_polyveck)))
@@ -426,8 +426,8 @@ __contract__(
  *
  * Returns number of 1 bits.
  **************************************************/
-MLD_MUST_CHECK_RETURN_VALUE
 MLD_INTERNAL_API
+MLD_MUST_CHECK_RETURN_VALUE
 unsigned int mld_polyveck_make_hint(mld_polyveck *h, const mld_polyveck *v0,
                                     const mld_polyveck *v1)
 __contract__(
