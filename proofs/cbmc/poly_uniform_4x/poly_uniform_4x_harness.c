72a234ab358c850fc4388da7fa96747cd6f2bdc7
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-02-09 14:43:58 +0800

    format: Apply clang-format 21 formatting changes
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- proofs/cbmc/poly_uniform_4x/poly_uniform_4x_harness.c
+++ proofs/cbmc/poly_uniform_4x/poly_uniform_4x_harness.c
@@ -9,7 +9,7 @@ void harness(void)
   mld_poly *r1;
   mld_poly *r2;
   mld_poly *r3;
-  uint8_t(*seed)[MLD_ALIGN_UP(MLDSA_SEEDBYTES + 2)];
+  uint8_t (*seed)[MLD_ALIGN_UP(MLDSA_SEEDBYTES + 2)];
 
   mld_poly_uniform_4x(r0, r1, r2, r3, seed);
 }
