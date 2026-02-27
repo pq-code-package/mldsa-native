24d5679dda0195fb62bba21ecf39f467f0cb7850
willieyz <willie.zhao@chelpis.com>
2026-02-27 14:50:01 +0800

    CBMC/AArch64: Port native Keccak F1600 CBMC proofs
    This commit adds the CBMC proofs for the native AArch64 Keccak routines from
    mlkem-native. This shows that the native contracts fulfills our API contracts.
    
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

---
+++ proofs/cbmc/keccak_f1600_x4_native_aarch64_v84a/keccak_f1600_x4_native_aarch64_v84a_harness.c
@@ -0,0 +1,14 @@
+// Copyright (c) The mlkem-native project authors
+// Copyright (c) The mldsa-native project authors
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+#include <stdint.h>
+
+int mld_keccak_f1600_x4_native(uint64_t *state);
+
+void harness(void)
+{
+  uint64_t *s;
+  int t = mld_keccak_f1600_x4_native(s);
+}
