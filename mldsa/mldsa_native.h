flynd/forked-mldsa-native

Add config to use only the internal API
Make it possible to exclude the wrapper APIs if not needed and build
only the internal API functions.

Signed-off-by: Anders Sonmark <Anders.Sonmark@axis.com>

mldsa/mldsa_native.h
@@ -244,6 +244,7 @@ int MLD_API_NAMESPACE(keypair_internal)(
 #endif
 );
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        crypto_sign_keypair
  *
@@ -277,6 +278,7 @@ int MLD_API_NAMESPACE(keypair)(
     MLD_CONFIG_CONTEXT_PARAMETER_TYPE context
 #endif
 );
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
 
 #if !defined(MLD_CONFIG_NO_SIGN_API)
@@ -326,6 +328,7 @@ int MLD_API_NAMESPACE(signature_internal)(
 #endif
 );
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        crypto_sign_signature
  *
@@ -441,6 +444,7 @@ int MLD_API_NAMESPACE(sign)(
     MLD_CONFIG_CONTEXT_PARAMETER_TYPE context
 #endif
 );
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 
 #if !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -482,6 +486,7 @@ int MLD_API_NAMESPACE(verify_internal)(
 #endif
 );
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        crypto_sign_verify
  *
@@ -586,6 +591,7 @@ int MLD_API_NAMESPACE(open)(
     MLD_CONFIG_CONTEXT_PARAMETER_TYPE context
 #endif
 );
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
 
 /*************************************************
@@ -605,6 +611,7 @@ int MLD_API_NAMESPACE(open)(
 #define MLD_PREHASH_SHAKE_128 11
 #define MLD_PREHASH_SHAKE_256 12
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 /*************************************************
  * Name:        crypto_sign_signature_pre_hash_internal
@@ -781,10 +788,12 @@ int MLD_API_NAMESPACE(verify_pre_hash_shake256)(
 #endif
 );
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 
 /* Maximum formatted domain separation message length */
 #define MLD_DOMAIN_SEPARATION_MAX_BYTES (2 + 255 + 11 + 64)
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        mld_prepare_domain_separation_prefix
  *
@@ -866,6 +875,7 @@ int MLD_API_NAMESPACE(pk_from_sk)(
 #endif
 );
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 
 #ifdef __cplusplus
 }

mldsa/src/fips202/fips202.c
@@ -260,6 +260,7 @@ void mld_shake256_release(mld_shake256ctx *state)
   mld_zeroize(state, sizeof(mld_shake256ctx));
 }
 
+#if !defined(MLD_CONFIG_NO_KEYPAIR_API) || !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 MLD_INTERNAL_API
 void mld_shake256(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen)
 {
@@ -271,5 +272,6 @@ void mld_shake256(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen)
   mld_shake256_squeeze(out, outlen, &state);
   mld_shake256_release(&state);
 }
+#endif /* !MLD_CONFIG_NO_KEYPAIR_API || !MLD_CONFIG_INTERNAL_API_ONLY */
 
 #endif /* !MLD_CONFIG_MULTILEVEL_NO_SHARED */

mldsa/src/fips202/fips202.h
@@ -221,6 +221,7 @@ __contract__(
   assigns(memory_slice(state, sizeof(mld_shake256ctx)))
 );
 
+#if !defined(MLD_CONFIG_NO_KEYPAIR_API) || !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 #define mld_shake256 MLD_NAMESPACE(shake256)
 /*************************************************
  * Name:        mld_shake256
@@ -241,5 +242,6 @@ __contract__(
   requires(memory_no_alias(out, outlen))
   assigns(memory_slice(out, outlen))
 );
+#endif /* !MLD_CONFIG_NO_KEYPAIR_API || !MLD_CONFIG_INTERNAL_API_ONLY */
 
 #endif /* !MLD_FIPS202_FIPS202_H */

mldsa/src/sign.c
@@ -364,6 +364,7 @@ cleanup:
   return mld_check_pct(pk, sk, context);
 }
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 #if !defined(MLD_CONFIG_NO_RANDOMIZED_API)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
@@ -387,6 +388,7 @@ cleanup:
   return ret;
 }
 #endif /* !MLD_CONFIG_NO_RANDOMIZED_API */
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
 
 #if !defined(MLD_CONFIG_NO_SIGN_API) || !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -856,6 +858,7 @@ cleanup:
   return ret;
 }
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 #if !defined(MLD_CONFIG_NO_RANDOMIZED_API)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
@@ -975,6 +978,7 @@ int mld_sign(uint8_t *sm, size_t *smlen, const uint8_t *m, size_t mlen,
   return ret;
 }
 #endif /* !MLD_CONFIG_NO_RANDOMIZED_API */
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 
 #if !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -1106,6 +1110,7 @@ cleanup:
   return ret;
 }
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
 int mld_sign_verify(const uint8_t *sig, size_t siglen, const uint8_t *m,
@@ -1189,8 +1194,10 @@ cleanup:
 
   return ret;
 }
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
@@ -1511,6 +1518,7 @@ cleanup:
   return ret;
 }
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 
 /* To facilitate single-compilation-unit (SCU) builds, undefine all macros.
  * Don't modify by hand -- this is auto-generated by scripts/autogen. */

mldsa/src/sign.h
@@ -127,6 +127,7 @@ __contract__(
           return_value == MLD_ERR_OUT_OF_MEMORY || return_value == MLD_ERR_RNG_FAIL)
 );
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        mld_sign_keypair
  *
@@ -160,6 +161,7 @@ __contract__(
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL ||
           return_value == MLD_ERR_OUT_OF_MEMORY || return_value == MLD_ERR_RNG_FAIL)
 );
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
 
 #if !defined(MLD_CONFIG_NO_SIGN_API)
@@ -221,6 +223,7 @@ __contract__(
   ensures(return_value != 0 ==> *siglen == 0)
 );
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        mld_sign_signature
  *
@@ -355,6 +358,7 @@ __contract__(
            || return_value == MLD_ERR_OUT_OF_MEMORY
            || return_value == MLD_ERR_RNG_FAIL))
 );
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 
 #if !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -402,6 +406,7 @@ __contract__(
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL || return_value == MLD_ERR_OUT_OF_MEMORY)
 );
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 /*************************************************
  * Name:        mld_sign_verify
  *
@@ -516,8 +521,10 @@ __contract__(
   assigns(memory_slice(mlen, sizeof(size_t)))
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL || return_value == MLD_ERR_OUT_OF_MEMORY)
 );
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
 
+#if !defined(MLD_CONFIG_INTERNAL_API_ONLY)
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 /*************************************************
  * Name:        mld_sign_signature_pre_hash_internal
@@ -822,5 +829,6 @@ __contract__(
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL || return_value == MLD_ERR_OUT_OF_MEMORY)
 );
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
+#endif /* !MLD_CONFIG_INTERNAL_API_ONLY */
 
 #endif /* !MLD_SIGN_H */

pq-code-package/mldsa-native

mldsa/mldsa_native.h
@@ -244,6 +244,7 @@ int MLD_API_NAMESPACE(keypair_internal)(
 #endif
 );
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        crypto_sign_keypair
  *
@@ -277,6 +278,7 @@ int MLD_API_NAMESPACE(keypair)(
     MLD_CONFIG_CONTEXT_PARAMETER_TYPE context
 #endif
 );
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
 
 #if !defined(MLD_CONFIG_NO_SIGN_API)
@@ -326,6 +328,7 @@ int MLD_API_NAMESPACE(signature_internal)(
 #endif
 );
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        crypto_sign_signature
  *
@@ -441,6 +444,7 @@ int MLD_API_NAMESPACE(sign)(
     MLD_CONFIG_CONTEXT_PARAMETER_TYPE context
 #endif
 );
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 
 #if !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -482,6 +486,7 @@ int MLD_API_NAMESPACE(verify_internal)(
 #endif
 );
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        crypto_sign_verify
  *
@@ -586,6 +591,7 @@ int MLD_API_NAMESPACE(open)(
     MLD_CONFIG_CONTEXT_PARAMETER_TYPE context
 #endif
 );
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
 
 /*************************************************
@@ -605,6 +611,7 @@ int MLD_API_NAMESPACE(open)(
 #define MLD_PREHASH_SHAKE_128 11
 #define MLD_PREHASH_SHAKE_256 12
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 /*************************************************
  * Name:        crypto_sign_signature_pre_hash_internal
@@ -781,10 +788,12 @@ int MLD_API_NAMESPACE(verify_pre_hash_shake256)(
 #endif
 );
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 
 /* Maximum formatted domain separation message length */
 #define MLD_DOMAIN_SEPARATION_MAX_BYTES (2 + 255 + 11 + 64)
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        mld_prepare_domain_separation_prefix
  *
@@ -866,6 +875,7 @@ int MLD_API_NAMESPACE(pk_from_sk)(
 #endif
 );
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 
 #ifdef __cplusplus
 }

mldsa/src/fips202/fips202.c
@@ -262,6 +262,7 @@ void mld_shake256_release(mld_shake256ctx *state)
   mld_zeroize(state, sizeof(mld_shake256ctx));
 }
 
+#if !defined(MLD_CONFIG_NO_KEYPAIR_API) || !defined(MLD_CONFIG_CORE_API_ONLY)
 MLD_INTERNAL_API
 void mld_shake256(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen)
 {
@@ -273,5 +274,6 @@ void mld_shake256(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen)
   mld_shake256_squeeze(out, outlen, &state);
   mld_shake256_release(&state);
 }
+#endif /* !MLD_CONFIG_NO_KEYPAIR_API || !MLD_CONFIG_CORE_API_ONLY */
 
 #endif /* !MLD_CONFIG_MULTILEVEL_NO_SHARED */

mldsa/src/fips202/fips202.h
@@ -220,6 +220,7 @@ __contract__(
   assigns(memory_slice(state, sizeof(mld_shake256ctx)))
 );
 
+#if !defined(MLD_CONFIG_NO_KEYPAIR_API) || !defined(MLD_CONFIG_CORE_API_ONLY)
 #define mld_shake256 MLD_NAMESPACE(shake256)
 /*************************************************
  * Name:        mld_shake256
@@ -240,5 +241,6 @@ __contract__(
   requires(memory_no_alias(out, outlen))
   assigns(memory_slice(out, outlen))
 );
+#endif /* !MLD_CONFIG_NO_KEYPAIR_API || !MLD_CONFIG_CORE_API_ONLY */
 
 #endif /* !MLD_FIPS202_FIPS202_H */

mldsa/src/sign.c
@@ -362,6 +362,7 @@ cleanup:
   return mld_check_pct(pk, sk, context);
 }
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 #if !defined(MLD_CONFIG_NO_RANDOMIZED_API)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
@@ -385,6 +386,7 @@ cleanup:
   return ret;
 }
 #endif /* !MLD_CONFIG_NO_RANDOMIZED_API */
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
 
 #if !defined(MLD_CONFIG_NO_SIGN_API) || !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -893,6 +895,7 @@ cleanup:
   return ret;
 }
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 #if !defined(MLD_CONFIG_NO_RANDOMIZED_API)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
@@ -1013,6 +1016,7 @@ int mld_sign(uint8_t *sm, size_t *smlen, const uint8_t *m, size_t mlen,
   return ret;
 }
 #endif /* !MLD_CONFIG_NO_RANDOMIZED_API */
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 
 #if !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -1144,6 +1148,7 @@ cleanup:
   return ret;
 }
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
 int mld_sign_verify(const uint8_t *sig, size_t siglen, const uint8_t *m,
@@ -1228,8 +1233,10 @@ cleanup:
 
   return ret;
 }
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 MLD_MUST_CHECK_RETURN_VALUE
 MLD_EXTERNAL_API
@@ -1560,6 +1567,7 @@ cleanup:
   return ret;
 }
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 
 /* To facilitate single-compilation-unit (SCU) builds, undefine all macros.
  * Don't modify by hand -- this is auto-generated by scripts/autogen. */

mldsa/src/sign.h
@@ -126,6 +126,7 @@ __contract__(
           return_value == MLD_ERR_OUT_OF_MEMORY || return_value == MLD_ERR_RNG_FAIL)
 );
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        mld_sign_keypair
  *
@@ -159,6 +160,7 @@ __contract__(
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL ||
           return_value == MLD_ERR_OUT_OF_MEMORY || return_value == MLD_ERR_RNG_FAIL)
 );
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
 
 #if !defined(MLD_CONFIG_NO_SIGN_API)
@@ -220,6 +222,7 @@ __contract__(
   ensures(return_value != 0 ==> *siglen == 0)
 );
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        mld_sign_signature
  *
@@ -354,6 +357,7 @@ __contract__(
            || return_value == MLD_ERR_OUT_OF_MEMORY
            || return_value == MLD_ERR_RNG_FAIL))
 );
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 
 #if !defined(MLD_CONFIG_NO_VERIFY_API)
@@ -401,6 +405,7 @@ __contract__(
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL || return_value == MLD_ERR_OUT_OF_MEMORY)
 );
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 /*************************************************
  * Name:        mld_sign_verify
  *
@@ -515,8 +520,10 @@ __contract__(
   assigns(memory_slice(mlen, sizeof(size_t)))
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL || return_value == MLD_ERR_OUT_OF_MEMORY)
 );
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 #endif /* !MLD_CONFIG_NO_VERIFY_API */
 
+#if !defined(MLD_CONFIG_CORE_API_ONLY)
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 /*************************************************
  * Name:        mld_sign_signature_pre_hash_internal
@@ -821,5 +828,6 @@ __contract__(
   ensures(return_value == 0 || return_value == MLD_ERR_FAIL || return_value == MLD_ERR_OUT_OF_MEMORY)
 );
 #endif /* !MLD_CONFIG_NO_KEYPAIR_API */
+#endif /* !MLD_CONFIG_CORE_API_ONLY */
 
 #endif /* !MLD_SIGN_H */