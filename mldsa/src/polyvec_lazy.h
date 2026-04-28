0a28caf5e378a34e8b5a2e64da6d1c5936315be2
Matthias J. Kannwischer <matthias@zerorisc.com>
2026-04-28 13:03:22 +0800

    CBMC: Add contracts and proofs for s2hat unpack/get_poly
    Signed-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>

--- mldsa/src/polyvec_lazy.h
+++ mldsa/src/polyvec_lazy.h
@@ -171,6 +171,13 @@ __contract__(
 static MLD_INLINE void mld_unpack_sk_s2hat_eager(
     mld_sk_s2hat_eager *s2,
     const uint8_t packed_s2[MLDSA_K * MLDSA_POLYETA_PACKEDBYTES])
+__contract__(
+  requires(memory_no_alias(s2, sizeof(mld_sk_s2hat_eager)))
+  requires(memory_no_alias(packed_s2, MLDSA_K * MLDSA_POLYETA_PACKEDBYTES))
+  assigns(memory_slice(s2, sizeof(mld_sk_s2hat_eager)))
+  ensures(forall(k1, 0, MLDSA_K,
+    array_abs_bound(s2->vec.vec[k1].coeffs, 0, MLDSA_N, MLD_NTT_BOUND)))
+)
 {
   mld_polyveck_unpack_eta(&s2->vec, packed_s2);
   mld_polyveck_ntt(&s2->vec);
@@ -180,23 +187,38 @@ static MLD_INLINE void mld_unpack_sk_s2hat_eager(
 static MLD_INLINE void mld_sk_s2hat_get_poly_eager(mld_poly *buf,
                                                    const mld_sk_s2hat_eager *s2,
                                                    unsigned int i)
-{
-  *buf = s2->vec.vec[i];
-}
+__contract__(
+  requires(memory_no_alias(buf, sizeof(mld_poly)))
+  requires(memory_no_alias(s2, sizeof(mld_sk_s2hat_eager)))
+  requires(i < MLDSA_K)
+  requires(array_abs_bound(s2->vec.vec[i].coeffs, 0, MLDSA_N, MLD_NTT_BOUND))
+  assigns(memory_slice(buf, sizeof(mld_poly)))
+  ensures(array_abs_bound(buf->coeffs, 0, MLDSA_N, MLD_NTT_BOUND))
+) { *buf = s2->vec.vec[i]; }
 #endif /* !MLD_CONFIG_NO_SIGN_API */
 #endif /* !MLD_CONFIG_REDUCE_RAM || MLD_UNIT_TEST */
 #if defined(MLD_CONFIG_REDUCE_RAM) || defined(MLD_UNIT_TEST)
 static MLD_INLINE void mld_unpack_sk_s2hat_lazy(
     mld_sk_s2hat_lazy *s2,
     const uint8_t packed_s2[MLDSA_K * MLDSA_POLYETA_PACKEDBYTES])
-{
-  s2->packed = packed_s2;
-}
+__contract__(
+  requires(memory_no_alias(s2, sizeof(mld_sk_s2hat_lazy)))
+  assigns(memory_slice(s2, sizeof(mld_sk_s2hat_lazy)))
+  ensures(s2->packed == old(packed_s2))
+) { s2->packed = packed_s2; }
 
 #if !defined(MLD_CONFIG_NO_SIGN_API)
 static MLD_INLINE void mld_sk_s2hat_get_poly_lazy(mld_poly *buf,
                                                   const mld_sk_s2hat_lazy *s2,
                                                   unsigned int i)
+__contract__(
+  requires(memory_no_alias(buf, sizeof(mld_poly)))
+  requires(memory_no_alias(s2, sizeof(mld_sk_s2hat_lazy)))
+  requires(i < MLDSA_K)
+  requires(memory_no_alias(s2->packed, MLDSA_K * MLDSA_POLYETA_PACKEDBYTES))
+  assigns(memory_slice(buf, sizeof(mld_poly)))
+  ensures(array_abs_bound(buf->coeffs, 0, MLDSA_N, MLD_NTT_BOUND))
+)
 {
   mld_polyeta_unpack(buf, s2->packed + i * MLDSA_POLYETA_PACKEDBYTES);
   mld_poly_ntt(buf);
