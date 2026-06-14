e573e0f92e6401de7117b3902d8ea2f69a91965e
Matthias J. Kannwischer <matthias@zerorisc.com>
2026-06-14 02:42:21 +0000

    SLOTHY: Pin to 0.2.2 to speed up CI re-optimization
    nixpkgs 26.05 ships slothy 0.2.0, which has a performance regression
    with ortools >= 9.7. Override pkgs.slothy to pin upstream tag 0.2.2,
    which fixes the regression and speeds up the AArch64 re-optimization
    path (and hence CI).
    
    Signed-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>

--- nix/slothy/default.nix
+++ nix/slothy/default.nix
@@ -9,14 +9,14 @@
 
 { pkgs }:
 
-pkgs.slothy
+# pkgs.slothy
 
-# pkgs.slothy.overrideAttrs (old: rec {
-#   version = "6d35cc147a0859f53f8bfc0d0f2ea3b947c8c4eb";
-#   src = pkgs.fetchFromGitHub {
-#     owner = "slothy-optimizer";
-#     repo = "slothy";
-#     rev = version;
-#     sha256 = "sha256-TplnMBjNvY7f8RTOwRWcv+cqxcRZ8KHx6toczNC5QGo=";
-#   };
-# })
+pkgs.slothy.overrideAttrs (old: rec {
+  version = "0.2.2";
+  src = pkgs.fetchFromGitHub {
+    owner = "slothy-optimizer";
+    repo = "slothy";
+    rev = version;
+    sha256 = "sha256-pyES6ithBVAFSVdjsM61kp6eeEUxNsLs7jdekpX+YuA=";
+  };
+})
