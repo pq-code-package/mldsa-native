a79e96e95461ac208820c3947483e8ad8b703545
Hanno Becker <beckphan@amazon.co.uk>
2026-09-08 08:32:45 +0000

    RV32IM: Add backend skeleton without any kernels
    Add `dev/riscv32/` and `mldsa/src/native/rv32im/` as the
    scaffolding for an RV32IM arithmetic backend.
    
    This adds the backend headers and makes the test build pick up
    future `rv32im` sources. The backend is still inert here:
    selection still requires both
    `MLD_CONFIG_USE_NATIVE_BACKEND_ARITH` and
    `MLD_CONFIG_ARITH_BACKEND_FILE`, and there are no native kernels
    yet.
    
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

---
+++ dev/riscv32/src/arith_native_rv32im.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) The mldsa-native project authors
+ * SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
+ */
+
+#ifndef MLD_NATIVE_RV32IM_SRC_ARITH_NATIVE_RV32IM_H
+#define MLD_NATIVE_RV32IM_SRC_ARITH_NATIVE_RV32IM_H
+
+#include "../../../cbmc.h"
+#include "../../../common.h"
+
+#endif /* !MLD_NATIVE_RV32IM_SRC_ARITH_NATIVE_RV32IM_H */
